d4e6c7dd7c11825083d8bdf15decdb7a34e7cad1
K <869268933@qq.com>
2018-02-06 23:28:11 +0800

    Add files via upload

---
+++ onenet_db.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 2018-01-24 10:55:44
+-- 服务器版本： 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `onenet_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `onenet_tb`
+--
+
+CREATE TABLE IF NOT EXISTS `onenet_tb` (
+  `Device_ID` int(11) NOT NULL,
+  `Temp` float NOT NULL,
+  ` Longitude` double NOT NULL,
+  ` Latitude` double NOT NULL,
+  PRIMARY KEY (`Device_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
